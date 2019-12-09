ba190e7e1f94d12369613475e9bbd7b39b5dd985
James Clancey <james.clancey@gmail.com>
2019-12-08 18:27:32 -0900

    Initial Commit! Day 8

---
+++ AdventOfCode/Days/Day8.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Days {
+	public class Day8 {
+
+		List<int> ParsePixels(string filePath)
+		{
+			var stringData = File.ReadAllText (filePath);
+			var pixels = stringData.Select (x=> int.Parse(x.ToString())).ToList ();
+			return pixels;
+		}
+		List<Layer> ParseLayers (List<int> pixels, int width, int height)
+		{
+			var pixelLength = width * height;
+			var layerCount = pixels.Count / pixelLength;
+			var layers = Enumerable.Range (0, layerCount).Select (x => pixels.Skip (pixelLength * x).Take (pixelLength))
+				.Select (x => new Layer { Pixels = x.ToList () }).ToList ();
+			return layers;
+			//pixels.Take(pixels)
+		}
+
+		public int Solve (string filePath, int width, int height)
+		{
+			var pixels = ParsePixels (filePath);
+			var layers = ParseLayers (pixels, width, height);
+			var answerLayer = layers.OrderBy (x => x.NumberOfZeros ()).First ();
+			return answerLayer.Value;
+		}
+		
+
+		public class Layer {
+
+			public List<int> Pixels = new List<int> ();
+			int zeroCount = -1;
+			public int NumberOfZeros()
+			{
+				if (zeroCount == -1)
+					zeroCount = Pixels.Count (x => x == 0);
+				return zeroCount;
+			}
+
+			public int Value => Pixels.Count (x => x == 1) * Pixels.Count (x => x == 2);
+		}
+	}
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using AdventOfCode.Days;
+
+namespace AdventOfCode {
+	class Program {
+		static void Main (string [] args)
+		{
+			Console.WriteLine ("Hello World!");
+			var path = Directory.GetCurrentDirectory ();
+			var dayAnswer = new Day8 ().Solve ("Inputs/Day8.txt", 25, 6);
+			Console.WriteLine ($"The answer is: {dayAnswer}");
+			Console.ReadKey ();
+		}
+	}
+}
