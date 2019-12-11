5e0cd6af21683cd1608b1ce973559cb0f87074b3
James Clancey <james.clancey@gmail.com>
2019-12-11 12:39:50 -0900

    Day 7 part 2

--- AdventOfCode/Days/Day7.cs
+++ AdventOfCode/Days/Day7.cs
@@ -7,16 +7,15 @@ namespace AdventOfCode.Days {
 		public Day7 ()
 		{
 		}
-
 		public override void SolvePart1 ()
 		{
 			var input = this.ReadInputString ();
-			var inputs = Enumerable.Range (0, 5).ToArray();
+			var inputs = Enumerable.Range (0, 5).ToArray ();
 			int maxOutput = 0;
-			foreach(var currentInputs in GetPermitation(inputs,0, inputs.Length - 1)) { 
+			foreach (var currentInputs in GetPermitation (inputs, 0, inputs.Length - 1)) {
 				var computers = GetComputers ();
 				var lastOutput = 0;
-				for (var i = 0; i < currentInputs.Count; i++){
+				for (var i = 0; i < currentInputs.Count; i++) {
 
 					lastOutput = computers [i].RunProgram (input, currentInputs [i], lastOutput);
 				}
@@ -24,45 +23,67 @@ namespace AdventOfCode.Days {
 			}
 			Console.WriteLine ($"Final Answer: {maxOutput}");
 		}
+
+
+
+		public override void SolvePart2 ()
+		{
+			var input = this.ReadInputString ();
+			var inputs = Enumerable.Range (5, 5).ToArray ();
+			int maxOutput = 0;
+			foreach (var currentInputs in GetPermitation (inputs, 0, inputs.Length - 1)) {
+				var computers = GetComputers ();
+				var lastOutput = 0;
+				var hasHalted = false;
+				while (!hasHalted) {
+					for (var i = 0; i < currentInputs.Count; i++) {
+						var cpu = computers [i];
+						lastOutput = cpu.RunProgram (input, currentInputs [i], lastOutput);
+						hasHalted = cpu.Halted;
+					}
+				}
+				maxOutput = Math.Max (lastOutput, maxOutput);
+			}
+			Console.WriteLine ($"Final Answer: {maxOutput}");
+
+		}
+
 		public List<IntCodeComputer> GetComputers () => Enumerable.Range (0, 5).Select (x => new IntCodeComputer ()).ToList ();
 
-		public void SwapTwoNumber (ref int a, ref int b)
+		public void SwapTwo<T> (ref T a, ref T b)
 		{
-			int temp = a;
+			T temp = a;
 			a = b;
 			b = temp;
 		}
-		public IEnumerable<List<int>> GetPermitation (int [] list, int k, int m)
+		public IEnumerable<List<T>> GetPermitation<T> (T [] list, int k, int m)
 		{
 			int i;
 			if (k == m) {
-				List<int> output = new List<int> ();
+				List<T> output = new List<T> ();
 				for (i = 0; i <= m; i++)
 					output.Add (list [i]);
 				yield return output;
 			} else
 				for (i = k; i <= m; i++) {
-					SwapTwoNumber (ref list [k], ref list [i]);
-					foreach(var perm in GetPermitation (list, k + 1, m))
+					SwapTwo (ref list [k], ref list [i]);
+					foreach (var perm in GetPermitation (list, k + 1, m))
 						yield return perm;
-					SwapTwoNumber (ref list [k], ref list [i]);
+					SwapTwo (ref list [k], ref list [i]);
 				}
 		}
 
-		public override void SolvePart2 ()
-		{
-			throw new NotImplementedException ();
-		}
-
 		public class IntCodeComputer {
-			List<int> input = new List<int> ();
+			public bool Halted { get; private set; }
+			List<int> input;
+			int inputCount = 0;
+			int output = 0;
+			public int ResetInput () => inputCount = 0;
 			public int RunProgram (string inputText, int phase, int arg)
 			{
-				this.input = inputText.Split (",").Select (int.Parse).ToList ();
-				Console.WriteLine ($"Phase:{phase} Input:{arg}");
-				currentIndex = 0;
-				int inputCount = 0;
-				var output = 0;
+				if ((input?.Count ?? 0) == 0)
+					this.input = inputText.Split (",").Select (int.Parse).ToList ();
+				//Console.WriteLine ($"Phase:{phase} Input:{arg}");
 
 				bool shouldContinue = true;
 				while (shouldContinue) {
@@ -71,6 +92,7 @@ namespace AdventOfCode.Days {
 					var opCode = codeResult.code;
 					var propCodes = codeResult.paramModes;
 					if (opCode == 99) {
+						Halted = true;
 						shouldContinue = false;
 						return output;
 					}
@@ -83,7 +105,8 @@ namespace AdventOfCode.Days {
 						var position = ReadCode ();
 						var value = ReadParameter (position, 0, propCodes);
 						output = value;
-						Console.WriteLine ($"Output:{value}");
+						//Console.WriteLine ($"Output:{value}");
+						return output;
 
 					} else if (opCode == 5) {
 						var p1 = ReadParameter (ReadCode (), 0, propCodes);
